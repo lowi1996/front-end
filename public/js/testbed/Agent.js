3f0548b3946dc79d08758b6c10841a0f5ac7fef3
arivas <arivas@ako.com>
2019-06-04 17:42:07 +0200

    adrian - accordion y panel

--- public/js/testbed/Agent.js
+++ public/js/testbed/Agent.js
@@ -48,3 +48,18 @@ function show_agent_info(){
 		agent_info.append(body)
 	}
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
