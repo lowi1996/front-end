6e132bb57b322400438fa1447b10abd444323bfe
arivas <arivas@ako.com>
2019-06-21 16:47:29 +0200

    dd

--- files/calibrate_ultrasonic_sensor.py
+++ files/calibrate_ultrasonic_sensor.py
@@ -11,11 +11,9 @@ try:
 	distance = 9999
 	while distance > 1 and distance != -1:
 		distance = sensors.read_distance()
-	car.set_angle(car.MIN_ANGLE)
+	car.set_speed(-20)
 	time.sleep(1)
-	car.set_angle(car.MAX_ANGLE)
-	time.sleep(1)
-	car.set_angle(car.DEFAULT_ANGLE)
+	car.set_speed(0)
 	print(json.dumps({"status": "success", "output": "Ultrasonic Sensor calibrated successfully"}))
 except Exception, e:
 	print("ERROR:{}".format(e))
