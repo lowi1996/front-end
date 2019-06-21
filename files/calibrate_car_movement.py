bfc598196d81ebc3c39b036f99a2c950a6a4fe2e
Adrián Rivas López <arivas@ako.com>
2019-06-21 10:45:19 +0200

    calibrate_car_movement.py

--- files/calibrate_car_movement.py
+++ files/calibrate_car_movement.py
@@ -3,25 +3,25 @@ from car_movement import CarMovement
 
 
 try:
-	car = Car()
-	angle = car.get_angle()
-	while angle > car.MIN_ANGLE:
-		angle -= 5
-		car.set_angle(angle)
-		time.sleep(0.1)
-	car.set_angle(car.DEFAULT_ANGLE)
-	angle = car.get_angle()
-	while angle < car.MAX_ANGLE:
-		angle += 5
-		car.set_angle(angle)
-		time.sleep(0.1)
-	car.set_angle(car.DEFAULT_ANGLE)
-	time.sleep(3)
-	car.set_speed(50)
-	time.sleep(2)
-	car.set_speed(-50)
-	time.sleep(2)
-	car.set_speed(0)
-	print(json.dumps({"status": "success", "output": "Car Movement calibrated successfully"}))
+    car = Car()
+    angle = car.get_angle()
+    while angle > car.MIN_ANGLE:
+        angle -= 5
+        car.set_angle(angle)
+        time.sleep(0.1)
+    car.set_angle(car.DEFAULT_ANGLE)
+    angle = car.get_angle()
+    while angle < car.MAX_ANGLE:
+        angle += 5
+        car.set_angle(angle)
+        time.sleep(0.1)
+    car.set_angle(car.DEFAULT_ANGLE)
+    time.sleep(3)
+    car.set_speed(50)
+    time.sleep(2)
+    car.set_speed(-50)
+    time.sleep(2)
+    car.set_speed(0)
+    print(json.dumps({"status": "success", "output": "Car wheels and motor calibrated successfully"}))
 except Exception, e:
-	print("ERROR:{}".format(e))
+    print("ERROR:{}".format(e))
