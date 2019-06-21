3bcaf57705697f06362c1d7fd6d3f1810077d6d7
Adrián <adrian96uni@gmail.com>
2019-06-21 09:24:15 +0200

    me he dejado el file.close()

--- files/get_start_position.py
+++ files/get_start_position.py
@@ -4,6 +4,7 @@ try:
   p_file = open("./config/car.config", "r")
   content = json.load(p_file)
   position = content["start"]
+  p_file.close()
   print(json.dumps({"Inicio": position}))
 except Exception, e:
   print("Error:{}".format(e))
