d812fd29c10ddfe475e2479e054f691eae1156d1
arivas <arivas@ako.com>
2019-06-26 18:53:06 +0200

    dd

--- files/continuous_follow_route.py
+++ files/continuous_follow_route.py
@@ -2,14 +2,18 @@ import sys
 import json
 import subprocess
 import requests
+from util import get_params
 
 
 try:
+    params = get_params(sys.argv)
     my_ip = subprocess.getoutput("hostname -I | awk '{print $1}'")
+    agent_id = params["agent_id"]
+
     while True:
         requests.post(
             "http://{}:8000/request_service".format(my_ip),
-            json={"service_id": "FOLLOW_ROUTE_FISICO"}
+            json={"service_id": "FOLLOW_ROUTE_FISICO", "agent_id": agent_id, "params": params}
         )
     print(json.dumps({"message": "Continuous follow route successfully executed"}))
 except Exception as e:
