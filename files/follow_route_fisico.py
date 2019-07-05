8bf5cf3a1f33866d26a84a8e14a533dfa29b643a
arivas <arivas@ako.com>
2019-07-05 17:35:07 +0200

    decision maker modificado

--- files/follow_route_fisico.py
+++ files/follow_route_fisico.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
         manager.start()
         sensors = Sensors()
         car = manager.CarMovement()
-        decision_maker = DecisionMaker(car, params["GESTION_SEMAFOROS_ip"], params["GESTION_SEMAFOROS_port"], params, CAR, frontend, q)
+        decision_maker = DecisionMaker(car, params, CAR, frontend, q)
         rfid_process = Process(target=read_RFID, args=(q, sensors, frontend,))
         distance_process = Process(target=read_distance, args=(q, sensors,))
         decision_process = Process(target=take_decision, args=(q,))
